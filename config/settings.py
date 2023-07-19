9b072626355ec88c4d1f4b0ae1c0848b7ccbc9a8
UsmonovAbdulloh <usmonovabdulloh1596@gmail.com>
2023-07-19 09:35:29 +0500

    sad

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app1',
+    'app2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
